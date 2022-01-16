6cc1bf80967f057e08666ff01a29f250186409b7
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2022-01-17 00:01:31 +0200

    init commit

--- trie_test.go
+++ trie_test.go
@@ -5,12 +5,35 @@ import (
 	"testing"
 )
 
+var t = InitTrie()
+var tr = InitTrie()
 
 func BenchmarkInsert(b *testing.B){
-  tr := InitTrie()
+
   for i := 0 ; i < b.N ; i++ {
-    tr.Insert("رقم" + strconv.Itoa(i))
+    t.Insert("رقم" + strconv.Itoa(i))
   }
 }
 
 
+func BenchmarkSearch( b *testing.B){
+  for i := 0 ; i < b.N; i++ {
+    t.Find("رقم" + strconv.Itoa(i))
+  }
+}
+
+
+func BenchmarkPut(b *testing.B){
+  for i := 0 ; i < b.N; i++ {
+    val := "رقم" + strconv.Itoa(i)
+    tr.Put(val, val)
+  }
+}
+
+
+func BenchmarkGet(b *testing.B){
+  for i := 0 ; i < b.N; i++ {
+    val := "رقم" + strconv.Itoa(i)
+    tr.Get(val)
+  }
+}
